c9007149415f152abf4c9bb20d009faa518816c8
kaede-1008 <s16t203@stu.kagawa-u.ac.jp>
2017-10-30 11:24:10 +0900

    front display&no frame

--- charapy.py
+++ charapy.py
@@ -2,20 +2,21 @@
 
 import sys
 from PyQt5.QtWidgets import(QApplication, QWidget, 
-    QLabel, QFrame, QHBoxLayout)
+                            QLabel, QFrame, 
+                            QHBoxLayout)
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 
-if __name__ == '__main__':
-
+def main():
     app = QApplication(sys.argv)
-    window = QWidget(None, Qt.FramelessWindowHint)
+    window = QWidget()
+    window.setWindowFlags(Qt.WindowStaysOnTopHint |Qt.FramelessWindowHint )
     window.setGeometry(1200, 565, 160, 160)
     pixmap = QPixmap("altina_02.png")
-
     label = QLabel('Label', window)
     label.setPixmap(pixmap)
-    
-
     window.show()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
