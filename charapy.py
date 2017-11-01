16c773262911758421b0a6abc1241728706b4dc3
kaede-1008 <s16t203@stu.kagawa-u.ac.jp>
2017-11-01 15:32:36 +0900

    induct function

--- charapy.py
+++ charapy.py
@@ -1,29 +1,40 @@
-# -*- coding: utf-8 -*-
-
 import sys
-from PyQt5.QtWidgets import(QApplication, QWidget, 
-                            QLabel, QFrame, 
-                            QHBoxLayout)
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
 import ctypes
 
-def getMonitorSize():
-    x = ctypes.windll.user32.GetSystemMetrics(0)
-    y = ctypes.windll.user32.GetSystemMetrics(1)
-    return x, y
 
-def main():
-    app = QApplication(sys.argv)
-    window = QWidget()
-    window.setWindowFlags(Qt.WindowStaysOnTopHint |Qt.FramelessWindowHint )
-    x, y = getMonitorSize()
-    window.setGeometry(x - 166, y - 203, 160, 160)
-    pixmap = QPixmap("altina_02.png")
-    label = QLabel('Label', window)
-    label.setPixmap(pixmap)
-    window.show()
-    sys.exit(app.exec_())
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowFlags(Qt.WindowStaysOnTopHint |Qt.FramelessWindowHint )
+        self.getmonitorsize()
+        self.initUI()
+
+    def initUI(self):
+        
+        # QPixmapオブジェクト作成
+        pixmap = QPixmap("altina_02.png")
+
+        # ラベルを作ってその中に画像を置く
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+        self.show()
+
+    def getmonitorsize(self):
+
+        x = ctypes.windll.user32.GetSystemMetrics(0)
+        y = ctypes.windll.user32.GetSystemMetrics(1)
+
+        self.setGeometry(x - 166, y - 203, 160, 160)
+        
+
 
 if __name__ == '__main__':
-    main()
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
